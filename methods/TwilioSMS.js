6023f67118b42f3a4ce059930c666e90574f588d
smdsaran <saranthuppaki056@gmail.com>
2022-04-19 19:55:06 +0530

    Added dotenv in twilio

--- methods/TwilioSMS.js
+++ methods/TwilioSMS.js
@@ -1,6 +1,9 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Find your Account SID and Auth Token at twilio.com/console
 // and set the environment variables. See http://twil.io/secure
+
+import "dotenv/config";
+
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 import twilio from "twilio";
